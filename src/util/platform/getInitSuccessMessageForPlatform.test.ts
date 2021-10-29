108a2e6ec35fe5d4e2b284ab347bf3b223ede6ad
Patrick Coffey <patrickcoffey48@gmail.com>
2021-10-29 15:33:10 -0500

    feat(30): add ability to specify init success messages per platform

---
+++ src/util/platform/getInitSuccessMessageForPlatform.test.ts
@@ -0,0 +1,35 @@
+import getInitSuccessMessageForPlatform from './getInitSuccessMessageForPlatform';
+
+// The unsafe assignment rule is disabled for this file because
+// it is important to use `expect.any`. There is no reason to test the
+// content of those messages, and doing so is especially odd due to the
+// character codes that come with chalk functions.
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+describe('getInitSuccessMessageForPlatform', () => {
+  it('can return init success log messages for a given platform', () => {
+    expect.assertions(1);
+    expect(getInitSuccessMessageForPlatform('drupal')).toEqual([
+      {
+        method: 'info',
+        message: expect.any(String),
+      },
+      {
+        method: 'verbose',
+        message: expect.any(String),
+      },
+      {
+        method: 'info',
+        message: expect.any(String),
+      },
+      {
+        method: 'verbose',
+        message: expect.any(String),
+      },
+    ]);
+  });
+
+  it('returns an empty array if the given platform does not correspond with any success messages', () => {
+    expect.assertions(1);
+    expect(getInitSuccessMessageForPlatform('java')).toEqual([]);
+  });
+});
