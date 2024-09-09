fcd957d5201de7112ce5eb206df67b0f1c1ac722
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-09-09 13:56:03 -0500

    feat: include the drupal starter as an available option

--- src/util/getAvailableStarters.test.ts
+++ src/util/getAvailableStarters.test.ts
@@ -9,7 +9,13 @@ describe('getAvailableStarters', () => {
         platform: 'none',
         platformMajorVersion: 1,
         repository: 'https://github.com/emulsify-ds/emulsify-starter',
-        checkout: 'develop',
+        checkout: 'main',
+      },
+      {
+        platform: 'drupal',
+        platformMajorVersion: 11,
+        repository: 'https://github.com/emulsify-ds/emulsify-drupal-starter',
+        checkout: 'main',
       },
     ];
 
--- src/util/getAvailableStarters.ts
+++ src/util/getAvailableStarters.ts
@@ -11,7 +11,13 @@ export default function getAvailableStarters(): EmulsifyStarter[] {
       platform: 'none',
       platformMajorVersion: 1,
       repository: 'https://github.com/emulsify-ds/emulsify-starter',
-      checkout: 'develop',
+      checkout: 'main',
+    },
+    {
+      platform: 'drupal',
+      platformMajorVersion: 11,
+      repository: 'https://github.com/emulsify-ds/emulsify-drupal-starter',
+      checkout: 'main',
     },
   ];
 }
